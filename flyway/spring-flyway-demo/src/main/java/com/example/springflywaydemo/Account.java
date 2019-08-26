ea9779b3f76fd0dcb959060ebfe820b92b8584dc
francesco <francesco@fedora.redhat.com>
2019-08-26 09:15:37 +0200

    added tutorial

---
+++ flyway/spring-flyway-demo/src/main/java/com/example/springflywaydemo/Account.java
@@ -0,0 +1,49 @@
+package com.example.springflywaydemo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    String userId;
+    String username;
+    String password;
+    String country;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
