7862f8b0985dc916b6e816525597f301af0bf23b
francesco <francesco@fedora.redhat.com>
2019-10-29 12:08:04 +0100

    vue example

--- front-ends/vue/src/main/java/com/example/testrest/CustomerController.java
+++ front-ends/vue/src/main/java/com/example/testrest/CustomerController.java
@@ -3,12 +3,14 @@ package com.example.testrest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+
 public class CustomerController
 {
     @Autowired
