e98c93f2b6e976d06d6044c41c2bc219b79f01a5
fmarchio <francesco@fedora.redhat.com>
2020-10-21 15:15:11 +0200

    commit

--- fuse/fuse-amq-artemis/src/main/java/com/sample/AppConfig.java
+++ fuse/fuse-amq-artemis/src/main/java/com/sample/AppConfig.java
@@ -1,9 +1,9 @@
 package com.sample;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.apache.camel.component.jms.JmsComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
