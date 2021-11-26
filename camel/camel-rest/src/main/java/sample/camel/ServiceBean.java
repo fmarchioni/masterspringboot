f8b9812059b48d8005fef6d3c30428ef98130670
fmarchio <francesco@fedora.redhat.com>
2021-11-26 15:59:50 +0100

    added camel rest example

---
+++ camel/camel-rest/src/main/java/sample/camel/ServiceBean.java
@@ -0,0 +1,12 @@
+package sample.camel;
+import org.springframework.stereotype.Component;
+@Component
+public class ServiceBean {
+
+    public User response(User user) {
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+    
+
+}
