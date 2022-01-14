f0b99e3352f7a1299638b23aceb6a8b2fd67053c
fmarchio <francesco@fedora.redhat.com>
2022-01-14 19:18:10 +0100

    updated rest demo

---
+++ helloworld/src/main/java/com/example/repository/CustomerRepository.java
@@ -0,0 +1,27 @@
+
+package com.example.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+import com.example.model.Customer;
+
+@Component
+public class CustomerRepository {
+  List<Customer> customerList = new ArrayList<Customer>();
+
+  @PostConstruct
+  public void init() {
+    customerList.add(new Customer(1, "frank"));
+    customerList.add(new Customer(2, "john"));
+  }
+
+  public List<Customer> getData() {
+    return customerList;
+  }
+  
+  public void saveCustomer(Customer c) {
+	  customerList.add(c);
+  }
+}
