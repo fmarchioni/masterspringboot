496ce65b4a6b3321754e5d57c4bd05e0abc6b31c
Francesco <marchioni.francesco@gmail.com>
2022-02-20 16:56:52 +0100

    update project

--- rest/swagger-demo/src/main/java/com/example/demorest/DemoApplication.java
+++ rest/swagger-demo/src/main/java/com/example/demorest/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demorest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 
