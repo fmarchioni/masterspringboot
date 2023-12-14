7020f433656745f8f426648fc81c20b48c1df0a4
fmarchioni <marchioni.francesco@gmail.com>
2023-12-14 18:39:10 +0100

    update project

--- rest/demo-rest-async/src/main/java/com/example/Application.java
+++ rest/demo-rest-async/src/main/java/com/example/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
+
 public class Application {
 
 
--- rest/demo-rest-async/src/test/java/com/example/AsyncControllerTest.java
+++ rest/demo-rest-async/src/test/java/com/example/AsyncControllerTest.java
@@ -12,9 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AsyncControllerTest {
-
-
-
 	@Autowired
 	private TestRestTemplate restTemplate;
 
